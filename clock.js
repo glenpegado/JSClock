7323cd1b5ef9f369253a90f1be5988e84734fb26
Glen Pegado <glenpegado@gmail.com>
2018-07-16 18:46:47 -0700

    added seconds function component

--- clock.js
+++ clock.js
@@ -1,8 +1,11 @@
+const secondHand = document.querySelector('.second-hand')
+
 function setDate() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = (seconds/ 60) * 360
-    console.log(seconds);
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const secondsDegrees = ((seconds/ 60) * 360)
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+    console.log(seconds)
 }
 
-setInterval(setDate, 1000);
+setInterval(setDate, 1000)
